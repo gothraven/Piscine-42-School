b498c41a0c87145cfd42050beaebdb41278d92d2
Safiy errahmane ZAGHBANE <szaghban@e2r13p11.42.fr>
2017-07-14 16:56:38 +0200

    move

--- ex11/ft_perso.h
+++ ex11/ft_perso.h
@@ -6,21 +6,23 @@
 /*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 13:40:47 by szaghban          #+#    #+#             */
-/*   Updated: 2017/07/14 13:52:46 by szaghban         ###   ########.fr       */
+/*   Updated: 2017/07/14 16:53:15 by szaghban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef FT_PERSP_H
-#define FT_PERSP_H
 
-#include <string.h>
+#ifndef FT_PERSO_H
+# define FT_PERSO_H
 
-#define SAVE_AUSTIN_POWERS "SAVE_AUSTIN_POWERS"
+# include <string.h>
 
-typedef struct perso {
-	char *name;
-	float life;
-	int age;
-	char *profession;
-} t_perso;
+# define SAVE_AUSTIN_POWERS "SAVE_AUSTIN_POWERS"
+
+typedef struct	s_perso
+{
+	char		*name;
+	float		life;
+	int			age;
+	char		*profession;
+}				t_perso;
 
 #endif
