855c10d427d4c4819258c926391b70bc023c58aa
Safiy errahmane ZAGHBANE <szaghban@e2r13p11.42.fr>
2017-07-14 15:42:48 +0200

    ex08

--- ex08/ft_spy.c
+++ ex08/ft_spy.c
@@ -6,16 +6,13 @@
 /*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 02:06:21 by szaghban          #+#    #+#             */
-/*   Updated: 2017/07/14 13:45:12 by szaghban         ###   ########.fr       */
+/*   Updated: 2017/07/14 15:41:53 by szaghban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
-
-
-
-
+#include <string.h>
 
 int		ft_keyword(char *str, char *keyword)
 {
@@ -23,21 +20,28 @@ int		ft_keyword(char *str, char *keyword)
 	int j;
 
 	i = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
-			|| str[i] == '\f' || str[i] == '\r')
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+			|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
 	j = 0;
-	while (str[i])
+	while (((keyword[j] == str[i]) || ((keyword[j] - 32) == str[i]))
+			&& keyword[j] && str[i] > 32)
 	{
-	
+		i++;
+		j++;
 	}
-
-	return (1);
+	if (!keyword[j])
+	{
+		while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+				|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+			i++;
+		if (!str[i])
+			return (1);
+	}
+	return (0);
 }
 
-
-
-int main(int argc, char *argv[])
+int		main(int argc, char *argv[])
 {
 	int i;
 
@@ -47,8 +51,8 @@ int main(int argc, char *argv[])
 		if (ft_keyword(argv[i], "president")
 				|| ft_keyword(argv[i], "attack")
 				|| ft_keyword(argv[i], "powers"))
-			write(1, "Alert!!!\n",9);
+			write(1, "Alert!!!\n", 9);
 		i++;
 	}
-	return 0;
+	return (0);
 }
