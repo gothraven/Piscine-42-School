2e67f5c12019905fd8486af725e9ba2a864a8c6d
Safiy errahmane ZAGHBANE <szaghban@e2r13p13.42.fr>
2017-07-16 23:16:51 +0200

    move the file in

--- ex00/sudoku.c
+++ ex00/sudoku.c
@@ -6,7 +6,7 @@
 /*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/15 16:50:39 by szaghban          #+#    #+#             */
-/*   Updated: 2017/07/16 23:11:00 by szaghban         ###   ########.fr       */
+/*   Updated: 2017/07/16 23:14:01 by szaghban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,13 @@ int		main(int argc, char *argv[])
 		if (is_sudoku(argv) && is_valid(argv))
 		{
 			sudoku = create_copy(argv);
-			if (*sudoku && find_solution(0, argv) && find_solution_reverse(81, sudoku))
+			if (*sudoku && find_solution(0, argv)
+					&& find_solution_reverse(81, sudoku))
 			{
 				if (is_same_solution(sudoku, argv))
 				{
 					print_sudoku(argv);
-					return free_sudoku(sudoku);
+					return (free_sudoku(sudoku));
 				}
 			}
 		}
