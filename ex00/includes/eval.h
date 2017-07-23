ec44dc0fb65db978cf682a4adf985fb309829df3
Safiy errahmane ZAGHBANE <szaghban@e2r13p13.42.fr>
2017-07-23 23:09:29 +0200

    move the files in

---
+++ ex00/includes/eval.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   eval.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/21 23:01:51 by szaghban          #+#    #+#             */
+/*   Updated: 2017/07/23 21:09:31 by szaghban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EVAL_H
+# define EVAL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define ERROR "PROBLEM - THAT IS NOT A NUMBER"
+
+int		eval_expr(char *str);
+char	*ft_init(char *str);
+int		ft_digits(void);
+int		ft_number(void);
+int		ft_level2(void);
+int		ft_level1(void);
+void	ft_nbr(long nb);
+void	ft_putnbr(int nb);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+
+#endif
