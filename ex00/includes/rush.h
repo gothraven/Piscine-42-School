fa1be25a2dbf7f73d8f2b06a44d1cf96bdbbdbfc
Safiy errahmane ZAGHBANE <szaghban@e2r13p13.42.fr>
2017-07-23 12:35:56 +0200

    move everything in

---
+++ ex00/includes/rush.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   eval.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/21 23:01:51 by szaghban          #+#    #+#             */
+/*   Updated: 2017/07/23 12:25:03 by szaghban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RUSH_H
+#define RUSH_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+void	ft_number(long nb);
+void	ft_putnbr(int nb);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+char	*ft_strdup(char *src);
+int		ft_strlen(char *str);
+
+#endif
