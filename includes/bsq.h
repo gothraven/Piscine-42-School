08b3779f72ad479eb815ebeee0ce7183e7125ee0
Safiy errahmane ZAGHBANE <szaghban@e2r13p13.42.fr>
2017-07-25 00:15:54 +0200

    starting the project

---
+++ includes/bsq.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsq.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szaghban <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/25 00:02:51 by szaghban          #+#    #+#             */
+/*   Updated: 2017/07/25 00:06:00 by szaghban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BSQ_H
+# define BSQ_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFFSIZE 2147483647
+# define TMPSIZE 1000
+
+#endif
