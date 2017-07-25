31d40ba195f30d705a8a5cb687854f1ce9c8f95e
Anas YAHYAOUI <anyahyao@e2r13p11.42.fr>
2017-07-25 09:29:22 +0200

    tata

---
+++ src/find_it.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_it.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anyahyao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/25 07:40:42 by anyahyao          #+#    #+#             */
+/*   Updated: 2017/07/25 09:13:34 by anyahyao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+int			min(int a, int b, int c)
+{
+	if (a < b)
+	{
+		if (a < c)
+			return (a);
+		return (c);
+	}
+	else if (c < b)
+		return (c);
+	return (b);
+}
+
+t_square	*ft_find_bis(t_obst **list, int *t1, int *t2, t_square *res)
+{
+	int		j;
+	int		h;
+	int		i;
+	int		k;
+	int		*swp;
+	t_obst	*l;
+
+	l = *list;
+	i = 1;
+	h = res->y;
+	k = res->x;
+	res->size = 1;
+	res->x = 0;
+	res->y = 0;
+	while (i < h)
+	{
+		j = 1;
+		t2[0] = 1;
+		if (l->x == 0 && l->y == i)
+		{
+			t2[0] = 0;
+			l = l->next;
+		}
+		while (j < k)
+		{
+			if (l->x == j && l->y == i)
+			{
+				t2[j] = 0;
+				l = l->next;
+			}
+			else
+				t2[j] = min(t2[j - 1], t1[j], t1[j - 1]) + 1;
+			if (t2[j] > res->size)
+			{
+				res->x = j;
+				res->y = i;
+				res->size = t2[j];
+			}
+			swp = t1;
+			t1 = t2;
+			t2 = swp;
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
+
+t_square	*ft_find(t_obst **l, int *t1, int *t2, t_square *res)
+{
+	int		i;
+	int		j;
+	t_obst	*list;
+
+	list = *l;
+	i = 1;
+	j = -1;
+	while (++j < res->x)
+		t1[j] = 1;
+	while (list->y == 0)
+	{
+		t1[list->x] = 0;
+		list = list->next;
+	}
+	return (ft_find_bis(&list, t1, t2, res));
+}
