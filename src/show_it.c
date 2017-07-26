b20a63293a4f1c6fe93f1ba509182733fd855392
Anas YAHYAOUI <anyahyao@e2r13p11.42.fr>
2017-07-26 06:29:38 +0200

    norm show

--- src/show_it.c
+++ src/show_it.c
@@ -6,22 +6,34 @@
 /*   By: anyahyao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/25 09:43:48 by anyahyao          #+#    #+#             */
-/*   Updated: 2017/07/26 04:21:19 by anyahyao         ###   ########.fr       */
+/*   Updated: 2017/07/26 06:28:59 by anyahyao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
+
+int		show_it(int j, char *str, t_map *m, int i)
+{
+	str[j] = '\n';
+	str[j + 1] = '\0';
+	write(1, str, m->width + 1);
+	return (i);
+}
+
+char	norm(t_map *m, int *pos)
+{
+	*pos = pos + 1;
+	return (m->obst);
+}
 
 int		show_it(t_square *s, int *tab, t_map *m)
 {
 	int		i;
 	int		j;
 	char	*str;
-	int pos;
-
+	int		pos;
 
 	pos = 0;
-	printf("x=%d y= %d  size=%d\n",s->x,s->y , s->size);
 	str = malloc(sizeof(char) * (m->width + 2));
 	i = 0;
 	while (i < m->height)
@@ -29,22 +41,16 @@ int		show_it(t_square *s, int *tab, t_map *m)
 		j = 0;
 		while (j < m->width)
 		{
-			if ( tab[pos] % m->width == j && tab[pos] / m->width == i)
-			{
-				str[j] = m->obst;
-				pos ++;
-			}
-			else if ((i > s->y - s->size && i <= s-> y) &&
+			if (tab[pos] % m->width == j && tab[pos] / m->width == i)
+				str[j] = norm(m, &pos);
+			else if ((i > s->y - s->size && i <= s->y) &&
 					(j > s->x - s->size && j <= s->x))
 				str[j] = m->full;
 			else
 				str[j] = m->empty;
 			j++;
 		}
-		str[j] = '\n';
-		str[j + 1] = '\0';
-		write(1, str, m->width + 1);
-		i++;
+		i = show_bis(j, str, m, i);
 	}
 	return (1);
 }
